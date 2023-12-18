c6aebd3f8caddeef40773b83f61c0f411f039944
Jose Antonio <Jose Antonio@LAPTOP-7NVR8CR1>
2023-12-18 12:03:12 +0100

    Commit de la conexionFTP detro del MenuEvent

--- ProyectoMulti/src/controlador/MenuEvent.java
+++ ProyectoMulti/src/controlador/MenuEvent.java
@@ -3,21 +3,26 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import conexion.ConexionFTP;
+import modelo.Modelo;
 import vista.Menu;
 import vista.VentanaEmail;
 import vista.VentanaFTP;
 
 public class MenuEvent implements ActionListener{
 	
-	Menu menu;
+	private Menu menu;
+	private Modelo modelo;
 
-	public MenuEvent(Menu menu) {
+	public MenuEvent(Menu menu,Modelo modelo) {
 		this.menu = menu;
+		this.modelo = modelo;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {	
 		if (e.getSource() == menu.getBtnFTP()) {
+			ConexionFTP conexionFTP = new ConexionFTP(modelo);
 			VentanaFTP vtnFtp = new VentanaFTP();
 			vtnFtp.frame.setVisible(true);
 			menu.frame.dispose();
